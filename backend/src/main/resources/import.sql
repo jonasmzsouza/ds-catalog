8560845b467648cadd4d9ebe1f915e8f68cfba5d
Jonas Souza <jonasmzsouza@gmail.com>
2022-03-19 16:31:47 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category(name) VALUES ('Book');
-INSERT INTO tb_category(name) VALUES ('Electronics');
-INSERT INTO tb_category(name) VALUES ('Computers');
+INSERT INTO tb_category(name, created_at) VALUES ('Book', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Electronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
