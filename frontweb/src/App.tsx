2ffef5db598504187d85f9e5ff18e0dd18fa24e0
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-16 23:14:20 -0300

    Routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
 
-import Catalog from 'pages/Catalog';
+import Routes from 'Routes';
 
 const App = () => {
   return (
-    <Catalog />
+    <Routes />
   );
 };
 
