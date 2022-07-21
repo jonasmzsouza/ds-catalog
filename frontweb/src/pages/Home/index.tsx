5d6a44a9772e5dfb1ff868c2328491d75bf10eb4
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-20 22:33:14 -0300

    chore(frontweb): Props

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const Home = () => {
           </div>
           <div>
             <Link to={'/products'}>
-              <ButtonIcon />
+              <ButtonIcon text="Inicie Agora a sua Busca" />
             </Link>
           </div>
         </div>
