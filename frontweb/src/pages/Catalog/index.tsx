d49639dfc93cdaf5373be3c9a4541c380777b711
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-22 23:29:57 -0300

    chore(frontweb): catalog title

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,3 +1,5 @@
+import './styles.css';
+
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
@@ -25,7 +27,12 @@ const Catalog = () => {
   };
 
   return (
-    <div className="container my-4">
+    <div className="container my-4 catalog-container">
+      <div className="row">
+        <div className="col catalog-title-container">
+          <h1>Catálogo de produtos</h1>
+        </div>
+      </div>
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to={'/products/1'}>
