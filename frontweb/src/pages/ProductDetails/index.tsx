ed7447412dec2e24924d3fce9f90e4fcebe5b5c9
Jonas Souza <jonasmzsouza@gmail.com>
2022-07-22 22:59:59 -0300

    chore(frontweb): productdetails styling

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,7 +1,35 @@
+import './styles.css';
+import ProductPrice from 'components/ProductPrice';
+import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg';
+
 const ProductDetails = () => {
-    return (
-      <h1>Página ProductDetails</h1>
-    )
-}
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <span>Voltar</span>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, enim.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
