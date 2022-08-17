d314bb5618b0d729956928ccfd5d046a207fd294
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-16 22:02:44 -0300

    chore(frontweb): add productformcard styling

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,7 +1,48 @@
+import './styles.css';
+
 const Form = () => {
   return (
-    <h1>Form</h1>
-  )
-}
+    <div className="base-card product-form-card">
+      <h2 className="product-form-title">Dados do Produto</h2>
+      <form action="">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="product-form-input-container">
+              <input
+                type="text"
+                name="name"
+                className="form-control base-input"
+                placeholder="Nome do Produto"
+              />
+            </div>
+            <div className="product-form-input-container">
+              <input
+                type="text"
+                name="price"
+                className="form-control base-input"
+                placeholder="Preço"
+              />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="product-form-textarea-container">
+              <textarea
+                name="description"
+                className="form-control base-input"
+                placeholder="Descrição"
+              ></textarea>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col product-form-buttons-container">
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-primary text-white">SALVAR</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
--- frontweb/src/pages/Admin/Products/index.tsx
+++ frontweb/src/pages/Admin/Products/index.tsx
@@ -2,16 +2,20 @@ import { Route, Switch } from 'react-router-dom';
 import Form from './Form';
 import List from './List';
 
+import './styles.css';
+
 const Products = () => {
   return (
-    <Switch>
-      <Route path={'/admin/products'} exact>
-        <List />
-      </Route>
-      <Route path={'/admin/products/:productId'}>
-        <Form />
-      </Route>
-    </Switch>
+    <div className="product-container">
+      <Switch>
+        <Route path={'/admin/products'} exact>
+          <List />
+        </Route>
+        <Route path={'/admin/products/:productId'}>
+          <Form />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
