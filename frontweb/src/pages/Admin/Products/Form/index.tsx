51207039873cc7c588d18cf63c9673531b45805e
Jonas Souza <jonasmzsouza@gmail.com>
2022-08-19 14:40:36 -0300

    chore(frontweb): retrieving categories from backend to select

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,8 +1,9 @@
 import { AxiosRequestConfig } from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
+import { Category } from 'types/category';
 import { Product } from 'types/product';
 import { requestBackend } from 'utils/requests';
 import './styles.css';
@@ -12,17 +13,13 @@ type UrlParams = {
 };
 
 const Form = () => {
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
-
   const { productId } = useParams<UrlParams>();
 
   const isEditing = productId !== 'create';
 
   const history = useHistory();
+
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
 
   const {
     register,
@@ -30,6 +27,12 @@ const Form = () => {
     formState: { errors },
     setValue,
   } = useForm<Product>();
+
+  useEffect(() => {
+    requestBackend({ url: '/categories' }).then((response) => {
+      setSelectCategories(response.data.content);
+    });
+  }, []);
 
   useEffect(() => {
     if (isEditing) {
@@ -93,7 +96,13 @@ const Form = () => {
             </div>
 
             <div className="product-form-input-container">
-              <Select options={options} classNamePrefix="product-form-select" isMulti />
+              <Select
+                options={selectCategories}
+                classNamePrefix="product-form-select"
+                isMulti
+                getOptionLabel={(category: Category) => category.name}
+                getOptionValue={(category: Category) => String(category.id)}
+              />
             </div>
 
             <div className="product-form-input-container">
