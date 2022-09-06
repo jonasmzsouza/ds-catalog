d73941642d6500c450e221dc8b906e1da3cb2ad0
Jonas Souza <jonasmzsouza@gmail.com>
2022-09-06 18:19:39 -0300

    chore(frontweb): describe block and test suite

--- frontweb/src/utils/__tests__/formatters.spec.ts
+++ frontweb/src/utils/__tests__/formatters.spec.ts
@@ -1,6 +1,25 @@
 import { formatPrice } from 'utils/formatters';
 
-test('formatPrice should format number pt-BR when given 10.1', () => {
-  const result = formatPrice(10.1);
-  expect(result).toEqual('10,10');
+describe('formatPrice for positive numbers', () => {
+  test('formatPrice should format number pt-BR when given 10.1', () => {
+    const result = formatPrice(10.1);
+    expect(result).toEqual('10,10');
+  });
+
+  test('formatPrice should format number pt-BR when given 0.1', () => {
+    const result = formatPrice(0.1);
+    expect(result).toEqual('0,10');
+  });
+});
+
+describe('formatPrice for non-positive numbers', () => {
+  test('formatPrice should format number pt-BR when given 0', () => {
+    const result = formatPrice(0);
+    expect(result).toEqual('0,00');
+  });
+
+  test('formatPrice should format number pt-BR when given -7.6', () => {
+    const result = formatPrice(-7.6);
+    expect(result).toEqual('-7,60');
+  });
 });
