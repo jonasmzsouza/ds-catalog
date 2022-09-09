240ccdc89e4636daf695e76dd49cf7354bdccc5a
Jonas Souza <jonasmzsouza@gmail.com>
2022-09-08 22:21:00 -0300

    test(frontweb): catalog - msw mock server, fixtures

--- frontweb/src/pages/Catalog/__test__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__test__/Catalog.spec.tsx
@@ -2,6 +2,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import history from 'utils/history';
 import Catalog from '..';
+import { server } from './fixtures';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test('should render Catalog with products', async () => {
   render(
